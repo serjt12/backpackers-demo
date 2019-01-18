0a6e763ee8a4015b9170814fde253a94697e9c8d
serjt12 <jandreus87@hotmail.com>
2019-01-18 11:55:09 -0500

    gitignore .next

--- .next/static/runtime/main.js
+++ .next/static/runtime/main.js
@@ -17956,25 +17956,12 @@ if (false) {}
 /***/ }),
 
 /***/ 0:
-/*!**************************************************************************************************************************************************************************************************************************************************************************************!*\
-  !*** multi ./utils/offline ./utils/offline ./utils/offline ./utils/offline ./utils/offline ./utils/offline ./utils/offline ./utils/offline ./utils/offline ./utils/offline ./utils/offline ./utils/offline ./utils/offline ./utils/offline ./node_modules/next/dist/client/next-dev ***!
-  \**************************************************************************************************************************************************************************************************************************************************************************************/
+/*!**********************************************************************!*\
+  !*** multi ./utils/offline ./node_modules/next/dist/client/next-dev ***!
+  \**********************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
-__webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
 __webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/utils/offline */"./utils/offline.js");
 module.exports = __webpack_require__(/*! /home/andres/Documents/web-dev/paralelo/backpackers-demo/node_modules/next/dist/client/next-dev */"./node_modules/next/dist/client/next-dev.js");
 
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cffdddee3da360ffa24c";
+/******/ 	var hotCurrentHash = "a261da76b3314abc9fe2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
