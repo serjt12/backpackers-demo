54e964ff84274b813cffacb527d9642525112d0e
serjt12 <jandreus87@hotmail.com>
2019-01-21 04:53:19 -0500

    add .next files

---
+++ .next/precache-manifest.33c00dbe1e70139e5231839acf6e5ec7.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c5671a057e240633049",
+    "url": "static/chunks/commons.3d4ac2506fa69b8724ce.js"
+  },
+  {
+    "revision": "b32085af0d8e53e23442",
+    "url": "static/runtime/webpack-42652fa8b82c329c0559.js"
+  },
+  {
+    "revision": "fc1d43e6b36455d87523",
+    "url": "static/runtime/main-bb2326425d757b9abb65.js"
+  },
+  {
+    "revision": "52618e180150e197019f",
+    "url": "static/MCqNRs1qrBf7FCLAH~tsr/pages/_app.js"
+  },
+  {
+    "revision": "0954b0c3c706b19e0284",
+    "url": "static/MCqNRs1qrBf7FCLAH~tsr/pages/country.js"
+  },
+  {
+    "revision": "680e7c5171d55bb334b5",
+    "url": "static/MCqNRs1qrBf7FCLAH~tsr/pages/destination.js"
+  },
+  {
+    "revision": "110b6385757c820e6834",
+    "url": "static/MCqNRs1qrBf7FCLAH~tsr/pages/home.js"
+  },
+  {
+    "revision": "a19c9c6fe99bcf741a57",
+    "url": "static/MCqNRs1qrBf7FCLAH~tsr/pages/index.js"
+  },
+  {
+    "revision": "b0243289d663197ca0cf",
+    "url": "static/MCqNRs1qrBf7FCLAH~tsr/pages/_error.js"
+  },
+  {
+    "revision": "e79e99cfb95bc6789598338659cc7b91",
+    "url": "react-loadable-manifest.json"
+  },
+  {
+    "revision": "a679c5d97ea83c3b00111fa70a246125",
+    "url": "build-manifest.json"
+  },
+  {
+    "revision": "f619d0139b4dffea3c912f0baef6c2e4",
+    "url": "static/img/android-chrome-144x144.png"
+  },
+  {
+    "revision": "30b207afd3b97dfeca557bf8fec28547",
+    "url": "static/img/android-chrome-192x192.png"
+  },
+  {
+    "revision": "2bada96314274804fd689a199d92d8bf",
+    "url": "static/img/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "f409a982c45f1da3e7860d28d6197956",
+    "url": "static/img/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "b3f3b877e4fbbcef4eafe74e7b3df09e",
+    "url": "static/img/bghero.jpg"
+  },
+  {
+    "revision": "aa9bff9925c89d02ba55dad6ddc6fbb1",
+    "url": "static/img/favicon.ico"
+  },
+  {
+    "revision": "bb1ca389e50c20e19c8a408c1b9ede74",
+    "url": "static/img/icons/adventure.png"
+  },
+  {
+    "revision": "7a925d8d3b023001fc8090f7c28caf2f",
+    "url": "static/img/icons/attractions.png"
+  },
+  {
+    "revision": "0ccf617d7172ef14fe80ced29d2644f2",
+    "url": "static/img/icons/bag.png"
+  },
+  {
+    "revision": "520281655ca6a509fc249ee53ae0eeca",
+    "url": "static/img/icons/best.png"
+  },
+  {
+    "revision": "c0380660a9e392ea28a38f61b6629531",
+    "url": "static/img/icons/bungee.png"
+  },
+  {
+    "revision": "3e3d59ed51ebd42df364a8ba469a4b6f",
+    "url": "static/img/icons/classes.png"
+  },
+  {
+    "revision": "b91b7f21c5b11b14a9b6c983b95df521",
+    "url": "static/img/icons/nobooking.png"
+  },
+  {
+    "revision": "a374ca992a60422c72088e9650532560",
+    "url": "static/img/icons/nomembership.png"
+  },
+  {
+    "revision": "90f416a4a54858f676e9eeede3b3fcd4",
+    "url": "static/img/icons/secure.png"
+  },
+  {
+    "revision": "66f642d4726d10e8f55dfa138b34188e",
+    "url": "static/img/icons/skydiving.png"
+  },
+  {
+    "revision": "9779324c5457b19bd654ff7399a19aee",
+    "url": "static/img/icons/surfing.png"
+  },
+  {
+    "revision": "5be090d16e4f5ad2f2ab2f0747ecd565",
+    "url": "static/img/icons/water.png"
+  },
+  {
+    "revision": "856e432654cc937183d1cf58de45a8cb",
+    "url": "static/img/logo.png"
+  },
+  {
+    "revision": "43ddb8390a3b8c0a940d1653b098e1b5",
+    "url": "static/img/splashscreen-icon-384x384.png"
+  }
+];
